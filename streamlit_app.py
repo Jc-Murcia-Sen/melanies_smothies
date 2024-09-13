9915240b144d6d8e90ad993910d420ba760c1d6e
Jc-Murcia-Sen <68298544+Jc-Murcia-Sen@users.noreply.github.com>
2024-09-13 08:48:31 -0500

    se aplica la busqueda de frutas usando el nuevo dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the snowpark Dataframe to a Pandas Dataframe so we can use de LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 # Se muestra la tabla obtenida en forma de lista seleccionable
 ingredients_list = st.multiselect(
@@ -64,6 +64,9 @@ if ingredients_list:
     # Union de diferentes ingredientes dentro de una misma variable
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         # --------------------------------------------------------------------
         # Seccion 2.A: Tabla de informacion nutricional obtenida de Frutyvice
